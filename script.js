510d441f287383015e7807025c31cc8fabf4d6d9
Denakpo Agba <67210181+agbagerard@users.noreply.github.com>
2025-02-13 18:54:10 -0500

    Identify item on click and get clue

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 initCategoryRow()
 initBoard()
 
+document.querySelector('button').addEventListener('click', buildCategories)
+
+//CREATE CATEGORY ROW
 function initCategoryRow() {
   let categoryRow = document.getElementById('category-row')
 
@@ -14,6 +17,7 @@ function initCategoryRow() {
   }
 }
 
+//CREATE CLUE BOARD
 function initBoard() {
   let board = document.getElementById('clue-board')
 
@@ -36,18 +40,73 @@ function initBoard() {
   }
 }
 
+//CALL API AND STORE DATA IN AN ARRAY
+let categoryArray = []
+
 function buildCategories() {
-  const fetchReq = fetch(
+  const fetchReq1 = fetch(
     `https://rithm-jeopardy.herokuapp.com/api/category?id=2`
   ).then((res) => res.json())
 
-  const allData = Promise.all([fetchReq])
+  const fetchReq2 = fetch(
+    `https://rithm-jeopardy.herokuapp.com/api/category?id=3`
+  ).then((res) => res.json())
+
+  const fetchReq3 = fetch(
+    `https://rithm-jeopardy.herokuapp.com/api/category?id=4`
+  ).then((res) => res.json())
+
+  const fetchReq4 = fetch(
+    `https://rithm-jeopardy.herokuapp.com/api/category?id=8`
+  ).then((res) => res.json())
+
+  const fetchReq5 = fetch(
+    `https://rithm-jeopardy.herokuapp.com/api/category?id=9`
+  ).then((res) => res.json())
+
+  const fetchReq6 = fetch(
+    `https://rithm-jeopardy.herokuapp.com/api/category?id=10`
+  ).then((res) => res.json())
+
+  const allData = Promise.all([
+    fetchReq1,
+    fetchReq2,
+    fetchReq3,
+    fetchReq4,
+    fetchReq5,
+    fetchReq6,
+  ])
 
   allData.then((res) => {
     console.log(res)
+    categoryArray = res
+    setCategories(categoryArray)
   })
 }
 
-function getClue() {
-  console.log('have a nice day')
+//LOAD CATEGORIES TO THE BOARD
+function setCategories(categoryArray) {
+  let element = document.getElementById('category-row')
+  let children = element.children
+  for (let i = 0; i < children.length; i++) {
+    children[i].innerHTML = categoryArray[i].title
+  }
+}
+
+//GET CLUES AND FIGURE OUT WHICH ITEM WAS CLICKED
+function getClue(event) {
+  let child = event.currentTarget
+  child.classList.add('clicked-box')
+  let boxValue = child.innerHTML.slice(1)
+  let parent = child.parentNode
+  let index = Array.prototype.findIndex.call(
+    parent.children,
+    (c) => c === child
+  )
+  let cluesList = categoryArray[index].clues
+  let clue = cluesList.find((obj) => {
+    return obj.value == boxValue
+  })
+  console.log(clue)
+  showQuestion(clue, child, boxValue)
 }
